d3e558b67ee6a821ec2283c8099ec183d0786ecd
HayaShalaby <124674435+HayaShalaby@users.noreply.github.com>
2023-05-11 22:52:18 +0300

    Update Location.h

--- Location.h
+++ Location.h
@@ -1,16 +1,20 @@
-/**
- * Project Untitled
- */
 
 
 #ifndef _LOCATION_H
 #define _LOCATION_H
 
 class Location {
-private: 
-    int id;
-    string name;
-    string remarks;
+    private: 
+        int id;
+        string name;
+        string remarks;
+     public: 
+        void setID(int ID);
+        int getID();
+        void setName(string n);
+        string getName();
+        void setRemarks(string r);
+        string getRemarks(); 
 };
 
 #endif //_LOCATION_H
