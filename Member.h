6c99b73afbdf0284d4630dfc0bd27bb3a68a20a8
HayaShalaby <124674435+HayaShalaby@users.noreply.github.com>
2023-05-11 22:51:27 +0300

    Update Member.h

--- Member.h
+++ Member.h
@@ -1,17 +1,21 @@
-/**
- * Project Untitled
- */
 
 
 #ifndef _MEMBER_H
 #define _MEMBER_H
 
 class Member {
-private: 
-    int id;
-    string name;
-    string address;
-    string contact;
+    private: 
+        int id;
+        string name;
+        string address;
+        string contact;
+     public: 
+        void setID(int ID);
+        int getID();
+        void setAddress(string a);
+        string getAddress();
+        void setContact(string c);
+        string getContact();
 };
 
 #endif //_MEMBER_H
