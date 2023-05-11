4a44dcec4c02466715c63951caf41d0dbd965298
HayaShalaby <124674435+HayaShalaby@users.noreply.github.com>
2023-05-11 22:50:08 +0300

    Update Photo.h

--- Photo.h
+++ Photo.h
@@ -1,18 +1,27 @@
-/**
- * Project Untitled
- */
 
 
 #ifndef _PHOTO_H
 #define _PHOTO_H
 
 class Photo {
-private: 
-    int id;
-    string title;
-    string remarks;
-    string type;
-    string privacy;
+    private: 
+        int id;
+        string title;
+        string remarks;
+        string type;
+        string privacy;
+     public: 
+        void setID(int ID);
+        int getID();
+        void setTitle(string t);
+        string getTitle();
+        void setRemarks(string r);
+        string getRemarks();
+        void setType(string ty);
+        string getType();
+        void setPrivacy(string p);
+        string getPrivacy();
+    
 };
 
 #endif //_PHOTO_H
