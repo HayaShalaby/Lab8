35b03151ac0abf19aefe9cc99e0d7b53fd54572e
HayaShalaby <124674435+HayaShalaby@users.noreply.github.com>
2023-05-11 22:47:21 +0300

    Update Tag.h

--- Tag.h
+++ Tag.h
@@ -1,16 +1,20 @@
-/**
- * Project Untitled
- */
 
 
 #ifndef _TAG_H
 #define _TAG_H
 
 class Tag {
-private: 
-    int id;
-    string name;
-    string remarks;
+    private: 
+        int id;
+        string name;
+        string remarks;
+    public: 
+        void setID(int ID);
+        int getID();
+        void setName(string n);
+        string getName();
+        void setRemarks(string r);
+        string getRemarks();  
 };
 
 #endif //_TAG_H
